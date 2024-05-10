7d49264faf616505d618a8179203888e32c6d19a
Daniel <daniel@Daniels-MacBook-Pro.local>
2024-05-10 16:04:05 +0200

    fix linter

--- cmd/website/main.go
+++ cmd/website/main.go
@@ -63,11 +63,17 @@ func main() {
 		}
 
 		component = templates.DetailsTemplate(d)
-		component.Render(context.Background(), w)
+		err = component.Render(context.Background(), w)
+		if err != nil {
+			fmt.Printf("Failed to render component: %v", err)
+		}
 
 	})
 
 	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Printf("Failed to start server: %v", err)
+	}
 
 }
